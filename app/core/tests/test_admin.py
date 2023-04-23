66557544eaa1bbacc8422b1dfb561fc02c9e88ec
Serkan Erkmen <serkan.erkmen@ericsson.com>
2023-04-23 21:35:03 +0300

    Unit test added for Django Admin Listing Users validation

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
+
+    # A special method to run before every single test. Naming is important
+    def setUp(self):
+        """Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        # Force authentication with user created
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        # URL to be pulled from Django Admin for User list
+        # Check Django documentation Reversing admin URLs
+        url = reverse('admin:core_user_changelist')
+        # Simulates HTTP GET request
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
