db15ed2fa802151c8088530f0799949e71795805
Serkan Erkmen <serkan.erkmen@ericsson.com>
2023-04-23 15:54:40 +0300

    Unit tests for customer user added + Customer User class implemented + Default user changed in settings.py + migration database done

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Use core.user as customer user model
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,38 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+# AbstractBaseUser:Contains functionality for authentication system
+# PermissionError:Contains funcationality for permission feature
+
+
+class UserManager(BaseUserManager):
+    """Manager for Users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionError):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # Determine if user can login as Django Admin
+    is_staff = models.BooleanField(default=True)
+    # Assign UserManager to custom User Class
+    objects = UserManager()
+    # The field that will be used for authentication.
+    # Replaces default user model authentication field to email
+    USERNAME_FIELD = 'email'
