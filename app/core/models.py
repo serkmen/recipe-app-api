7e5a37e18a8485465fdddcd8e131cbb2e75d1840
Serkan Erkmen <serkan.erkmen@ericsson.com>
2023-04-23 19:19:40 +0300

    Require e-mail input feature added

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,7 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin,
-)
+    )
 
 # AbstractBaseUser:Contains functionality for authentication system
 # PermissionError:Contains funcationality for permission feature
@@ -17,6 +16,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
